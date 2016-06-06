260f7cf34b247f2175b9bb16311d60195429368b
LightLink.io <i@vssol.fr>
2016-06-06 11:24:30 +0200

    Excel export more generic types

--- core/src/main/java/io/lightlink/excel/StreamingExcelExporter.java
+++ core/src/main/java/io/lightlink/excel/StreamingExcelExporter.java
@@ -1,12 +1,35 @@
 package io.lightlink.excel;
 
+/*
+ * #%L
+ * LightLink Core
+ * %%
+ * Copyright (C) 2015 - 2016 Vitaliy Shevchuk
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+
 import org.apache.commons.beanutils.BeanMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -42,7 +65,7 @@ public class StreamingExcelExporter {
         this.data = data;
     }
 
-    public void doExport(final ServletOutputStream out) throws IOException {
+    public void doExport(final OutputStream out) throws IOException {
 
         InputStream templateStream = template.openStream();
         ExcelStreamVisitor visitor = new WritingExcelStreamVisitor(getData(), getExportDateTimeFormat());
